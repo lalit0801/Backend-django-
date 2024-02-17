0f619968184e5986e0e3f7f372b9457f563abeec
Lalit <lalitdream999@gmail.com>
2024-02-17 22:28:09 +0530

    models intro

--- newpro/myapp/views.py
+++ newpro/myapp/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Feature
 
 # Create your views here.
 def index(request):
-    context={
-        'name': 'Lalit',
-        'age':'22',
-        'Nationality': 'INDIAN',
-    }
-    return render(request,'index.html',context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'lalit'
+    feature1.details= 'i am optimus'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'lalit'
+    feature2.details= 'i am optimus'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'lalit'
+    feature3.details= 'i am optimus'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'lalit'
+    feature4.details= 'i am optimus'
+
+    features =[feature1, feature2, feature3, feature4]
+    return render(request,'index.html',{'features': features})
 
 def counter(request):
     text=request.POST['text']
