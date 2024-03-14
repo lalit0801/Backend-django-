7882b863b6e61ac7dc95a39773e367e05f75b8fb
Lalit <lalitdream999@gmail.com>
2024-03-14 13:17:56 +0530

    .

--- DRF/backend/products/views.py
+++ DRF/backend/products/views.py
@@ -8,6 +8,9 @@ class ProductCreateAPIView(generics.CreateAPIView):
     queryset= Product.objects.all()
     serializer_class= ProductSerializer
 
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
+
 
 product_create_view= ProductCreateAPIView.as_view()
     
