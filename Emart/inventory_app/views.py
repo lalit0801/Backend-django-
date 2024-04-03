b46ddc4e6fd4b00e4d1186fa6a1a02e3e42de61c
Lalit <lalitdream999@gmail.com>
2024-04-03 23:23:17 +0530

    Update views.py

--- Emart/inventory_app/views.py
+++ Emart/inventory_app/views.py
@@ -1,9 +1,10 @@
-# from django.shortcuts import render
-# from rest_framework.permissions import IsAuthenticated
-# from .models import CartItem
-# from.serializers import InventorySerializer
-# from rest_framework import generics
-# from rest_framework.authentication import TokenAuthentication
-# from  cart_app.permissions import IsOwnerOrAdmin
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from .models import CartItem
+from.serializers import InventorySerializer
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from  cart_app.permissions import IsOwnerOrAdmin
 
 # # permissions yet to be included
+# 
